4e824e4bf39dcbe00d30b3214a3331b44c77725d
daoudacherif <daoudacherif4321@gmail.com>
2025-04-06 02:05:00 +0000

    db fixed

--- admin/includes/dbconnection.php
+++ admin/includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("mysql.hostinger.com", "u553063725_Daoudacherif", "Daoudacherif4321", "u553063725_italalu");
+$con = mysqli_connect("mysql.hostinger.com", "u451994146_root", "Daoudacherif4321", "u451994146_etstmc");
 
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
