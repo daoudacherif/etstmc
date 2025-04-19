d89bbcb150455911d82f2a056c84d807ceab2c15
daoudacherif4321 <daoudacherif4321@gmail.com>
2025-04-19 15:13:01 +0000

    button fixed

--- index.php
+++ index.php
@@ -82,34 +82,30 @@
         .btn-hero:hover, .btn-pricing:hover { background-color: #ca6702; }
 
    /* From Uiverse.io by barisdogansutcu */ 
-.button {
-  padding: 17px 40px;
-  border-radius: 50px;
-  cursor: pointer;
+/* From Uiverse.io by elijahgummer */ 
+button {
+  font: inherit;
+  background-color: #f0f0f0;
   border: 0;
-  background-color: white;
-  box-shadow: rgb(0 0 0 / 5%) 0 0 8px;
-  letter-spacing: 1.5px;
-  text-transform: uppercase;
-  font-size: 15px;
-  transition: all 0.5s ease;
+  color: #242424;
+  border-radius: 0.5em;
+  font-size: 1.35rem;
+  padding: 0.375em 1em;
+  font-weight: 600;
+  text-shadow: 0 0.0625em 0 #fff;
+  box-shadow: inset 0 0.0625em 0 0 #f4f4f4, 0 0.0625em 0 0 #efefef,
+    0 0.125em 0 0 #ececec, 0 0.25em 0 0 #e0e0e0, 0 0.3125em 0 0 #dedede,
+    0 0.375em 0 0 #dcdcdc, 0 0.425em 0 0 #cacaca, 0 0.425em 0.5em 0 #cecece;
+  transition: 0.15s ease;
+  cursor: pointer;
+}
+button:active {
+  translate: 0 0.225em;
+  box-shadow: inset 0 0.03em 0 0 #f4f4f4, 0 0.03em 0 0 #efefef,
+    0 0.0625em 0 0 #ececec, 0 0.125em 0 0 #e0e0e0, 0 0.125em 0 0 #dedede,
+    0 0.2em 0 0 #dcdcdc, 0 0.225em 0 0 #cacaca, 0 0.225em 0.375em 0 #cecece;
 }
 
-.button:hover {
-  letter-spacing: 3px;
-  background-color:  #0a9396;
-  color: hsl(0, 0%, 100%);
-  box-shadow:rgb(69, 225, 228) 0px 7px 29px 0px;
-}
-
-.button:active {
-  letter-spacing: 3px;
-  background-color: hsl(261deg 80% 48%);
-  color: hsl(0, 0%, 100%);
-  box-shadow: rgb(93 24 220) 0px 0px 0px 0px;
-  transform: translateY(10px);
-  transition: 100ms;
-}
 
     </style>
 </head>
