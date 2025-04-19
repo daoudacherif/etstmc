ecabc2708e0048fc313122a91bd98786a1659c5c
daoudacherif4321 <daoudacherif4321@gmail.com>
2025-04-19 16:07:46 +0000

    sidebar fixed

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -1,50 +1,92 @@
-<!--Menu latéral-->
 <div id="sidebar">
   <ul>
-    <li class="active"><a href="dashboard.php"><i class="icon icon-home"></i> <span>Tableau de bord</span></a> </li>
-    <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Catégorie</span></a>
+    <li class="active"><a href="dashboard.php"><i class="icon icon-home"></i> <span>Tableau de bord</span></a></li>
+
+    <!-- Gestion des produits -->
+    <li class="submenu">
+      <a href="#"><i class="icon icon-th-list"></i> <span>Catalogues</span></a>
       <ul>
-        <li><a href="add-category.php">Ajouter une catégorie</a></li>
-        <li><a href="manage-category.php">Gérer les catégories</a></li>
+        <li class="submenu">
+          <a href="#"><i class="icon icon-th"></i> Catégorie</a>
+          <ul>
+            <li><a href="add-category.php">Ajouter</a></li>
+            <li><a href="manage-category.php">Gérer</a></li>
+          </ul>
+        </li>
+        <li class="submenu">
+          <a href="#"><i class="icon icon-th-large"></i> Sous-catégorie</a>
+          <ul>
+            <li><a href="add-subcategory.php">Ajouter</a></li>
+            <li><a href="manage-subcategory.php">Gérer</a></li>
+          </ul>
+        </li>
+        <li class="submenu">
+          <a href="#"><i class="icon icon-tag"></i> Marque</a>
+          <ul>
+            <li><a href="add-brand.php">Ajouter</a></li>
+            <li><a href="manage-brand.php">Gérer</a></li>
+          </ul>
+        </li>
+        <li class="submenu">
+          <a href="#"><i class="icon icon-barcode"></i> Produit</a>
+          <ul>
+            <li><a href="add-product.php">Ajouter</a></li>
+            <li><a href="manage-product.php">Gérer</a></li>
+          </ul>
+        </li>
       </ul>
     </li>
-    <li class="submenu"> <a href="#"><i class="icon icon-inbox"></i> <span>Sous-catégorie</span></a>
+
+    <!-- Ventes -->
+    <li class="submenu">
+      <a href="#"><i class="icon-shopping-cart"></i> <span>Ventes</span></a>
       <ul>
-        <li><a href="add-subcategory.php">Ajouter une sous-catégorie</a></li>
-        <li><a href="manage-subcategory.php">Gérer les sous-catégories</a></li>
+        <li><a href="cart.php">Comptant <span class="label label-important"><?php echo htmlentities($cartcountcount);?></span></a></li>
+        <li><a href="dettecart.php">Terme <span class="label label-important"><?php echo htmlentities($cartcountcount);?></span></a></li>
+        <li><a href="return.php">Retour</a></li>
       </ul>
     </li>
-    <li class="submenu"> <a href="#"><i class="icon icon-file"></i> <span>Marque</span></a>
+
+    <!-- Fournisseurs -->
+    <li class="submenu">
+      <a href="#"><i class="icon icon-truck"></i> <span>Fournisseurs</span></a>
       <ul>
-        <li><a href="add-brand.php">Ajouter une marque</a></li>
-        <li><a href="manage-brand.php">Gérer les marques</a></li>
+        <li><a href="supplier.php">Liste fournisseurs</a></li>
+        <li><a href="arrival.php">Arrivages</a></li>
+        <li><a href="supplier-payments.php">Paiements</a></li>
       </ul>
     </li>
-    <li class="submenu"> <a href="#"><i class="icon icon-info-sign"></i> <span>Produit</span></a>
+
+    <!-- Clients -->
+    <li class="submenu">
+      <a href="#"><i class="icon icon-user"></i> <span>Clients</span></a>
       <ul>
-        <li><a href="add-product.php">Ajouter un produit</a></li>
-        <li><a href="manage-product.php">Gérer les produits</a></li>
+        <li><a href="client-account.php">Comptes clients</a></li>
+        <li><a href="customer-details.php">Détails clients</a></li>
       </ul>
     </li>
-    <li> <a href="inventory.php"><i class="icon icon-info-sign"></i> <span>Inventaire</span></a></li>
-    <li> <a href="cart.php"><i class="icon-shopping-cart"></i> <span>Comptant</span><span class="label label-important"><?php echo htmlentities($cartcountcount);?></span></a></li>
-    <li> <a href="dettecart.php"><i class="icon-shopping-cart"></i> <span>Terme</span><span class="label label-important"><?php echo htmlentities($cartcountcount);?></span></a></li>
-    <li> <a href="search.php"><i class="icon-search"></i> <span>Rechercher</span></a></li>
-    <li> <a href="transact.php"><i class="icon-search"></i> <span>Transactions</span></a></li>
-    <li> <a href="return.php"><i class="icon-search"></i> <span>Retour</span></a></li>
-    <li> <a href="invoice-search.php"><i class="icon-search"></i> <span>Rechercher une facture</span></a></li>
-    <li> <a href="arrival.php"><i class="icon-group"></i> <span>Arrivage</span></a></li>  
-    <li> <a href="supplier.php"><i class="icon-group"></i> <span>fournisseur</span></a></li>  
-    <li> <a href="supplier-payments.php"><i class="icon-group"></i> <span>payment fournisseur</span></a></li>  
-    
-    <li> <a href="client-account.php"><i class="icon-group"></i> <span>compte client</span></a></li>
-    <li> <a href="customer-details.php"><i class="icon-group"></i> <span>Détails client</span></a></li>
-    <li class="submenu"> <a href="#"><i class="icon icon-th-list"></i> <span>Rapports</span></a>
+
+    <!-- Transactions -->
+    <li class="submenu">
+      <a href="#"><i class="icon icon-exchange"></i> <span>Transactions</span></a>
       <ul>
-        <li><a href="stock-report.php">Rapport de stock</a></li>
-        <li><a href="sales-report.php">Rapport des ventes</a></li>
-        <li><a href="daily-repport.php">Rapport des journalier</a></li>
+        <li><a href="transact.php">Historique</a></li>
+        <li><a href="invoice-search.php">Recherche facture</a></li>
       </ul>
     </li>
+
+    <!-- Rapports -->
+    <li class="submenu">
+      <a href="#"><i class="icon icon-file"></i> <span>Rapports</span></a>
+      <ul>
+        <li><a href="stock-report.php">Stock</a></li>
+        <li><a href="sales-report.php">Ventes</a></li>
+        <li><a href="daily-repport.php">Journalier</a></li>
+      </ul>
+    </li>
+
+    <!-- Autres éléments -->
+    <li><a href="inventory.php"><i class="icon icon-hdd"></i> <span>Inventaire</span></a></li>
+    <li><a href="search.php"><i class="icon-search"></i> <span>Recherche</span></a></li>
   </ul>
 </div>
