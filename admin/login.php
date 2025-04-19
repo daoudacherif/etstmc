8ee904ad609a046dc7b098f73ec330833224ee33
daoudacherif4321 <daoudacherif4321@gmail.com>
2025-04-19 17:33:18 +0000

    login test

--- admin/login.php
+++ admin/login.php
@@ -67,7 +67,7 @@ if(isset($_POST['submit'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="css/bootstrap.min.css" />
     <link rel="stylesheet" href="css/bootstrap-responsive.min.css" />
-    <link rel="stylesheet" href="css/matrix-login.css" />
+   
     <link href="font-awesome/css/font-awesome.css" rel="stylesheet" />
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
     <style>
