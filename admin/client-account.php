95d85ee227751de7253ce0441e8d1f2a84c20f22
daoudacherif4321 <daoudacherif4321@gmail.com>
2025-04-20 22:30:31 +0000

    test 2

--- admin/client-account.php
+++ admin/client-account.php
@@ -42,8 +42,9 @@ $res = mysqli_query($con, $sql);
   <title>Compte Client</title>
   <?php include_once('includes/cs.php'); ?>
   <?php include_once('includes/responsive.php'); ?>
+  <?php include_once('includes/sidebar.php'); ?>
 <?php include_once('includes/header.php'); ?>
-<?php include_once('includes/sidebar.php'); ?>
+
 
 <div id="content">
   <div id="content-header">
