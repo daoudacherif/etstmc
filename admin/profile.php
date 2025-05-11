f4fab5163b5450c247bf50dc63919b3b26d15ab1
daoudacherif <daoudacherif4321@gmail.com>
2025-05-11 04:01:43 +0000

    profile new update

--- admin/profile.php
+++ admin/profile.php
@@ -4,28 +4,43 @@ error_reporting(0);
 include('includes/dbconnection.php');
 if (strlen($_SESSION['imsaid']==0)) {
   header('location:logout.php');
-  } else{
-    if(isset($_POST['submit']))
-  {
-    $adminid=$_SESSION['imsaid'];
-    $aname=$_POST['adminname'];
-  $mobno=$_POST['contactnumber'];
-  
-     $query=mysqli_query($con, "update tbladmin set AdminName ='$aname', MobileNumber='$mobno' where ID='$adminid'");
-    if ($query) {
+} else {
+  // Handle user creation
+  if(isset($_POST['createUser'])) {
+    $adminid = $_SESSION['imsaid'];
+    $username = $_POST['username'];
     
-    echo '<script>alert("Le profil de l\'administrateur a été mis à jour.")</script>';
-  }
-  else
-    {
-      echo '<script>alert("Quelque chose a mal tourné. Veuillez réessayer.")</script>';
+    // Check if the username is "saler" as required
+    if($username == 'saler') {
+      $password = md5($_POST['password']);
+      $adminname = $_POST['adminname'];
+      $mobileno = $_POST['mobileno'];
+      $email = $_POST['email'];
+      $regdate = date('Y-m-d H:i:s');
+      
+      // Check if username already exists
+      $check = mysqli_query($con, "SELECT ID FROM tbladmin WHERE UserName='$username'");
+      if(mysqli_num_rows($check) > 0) {
+        echo '<script>alert("Ce nom d\'utilisateur existe déjà. Veuillez réessayer.")</script>';
+      } else {
+        $query = mysqli_query($con, "INSERT INTO tbladmin(AdminName, UserName, MobileNumber, Email, Password, AdminRegdate) 
+                                     VALUES('$adminname', '$username', '$mobileno', '$email', '$password', '$regdate')");
+        if($query) {
+          echo '<script>alert("Nouvel utilisateur créé avec succès.")</script>';
+        } else {
+          echo '<script>alert("Échec de la création. Veuillez réessayer.")</script>';
+        }
+      }
+    } else {
+      echo '<script>alert("Vous ne pouvez créer que des utilisateurs avec le nom d\'utilisateur \"saler\".")</script>';
     }
   }
-  ?>
+?>
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-<title>Système de gestion des stocks || Profil</title>
+<title>Système de gestion des stocks || Gestion des utilisateurs</title>
 <?php include_once('includes/cs.php');?>
 <?php include_once('includes/responsive.php'); ?>
 
@@ -33,11 +48,10 @@ if (strlen($_SESSION['imsaid']==0)) {
 <?php include_once('includes/header.php');?>
 <?php include_once('includes/sidebar.php');?>
 
-
 <div id="content">
 <div id="content-header">
-  <div id="breadcrumb"> <a href="dashboard.php" title="Aller à l'accueil" class="tip-bottom"><i class="icon-home"></i> Accueil</a> <a href="profile.php" class="tip-bottom">Profil</a></div>
-  <h1>Profil</h1>
+  <div id="breadcrumb"> <a href="dashboard.php" title="Aller à l'accueil" class="tip-bottom"><i class="icon-home"></i> Accueil</a> <a href="manage-users.php" class="tip-bottom">Gestion des utilisateurs</a></div>
+  <h1>Gestion des utilisateurs</h1>
 </div>
 <div class="container-fluid">
   <hr>
@@ -45,59 +59,89 @@ if (strlen($_SESSION['imsaid']==0)) {
     <div class="span12">
       <div class="widget-box">
         <div class="widget-title"> <span class="icon"> <i class="icon-align-justify"></i> </span>
-          <h5>Profil</h5>
+          <h5>Créer un nouvel utilisateur</h5>
         </div>
         <div class="widget-content nopadding">
           <form method="post" class="form-horizontal">
-            <?php
-$adminid=$_SESSION['imsaid'];
-$ret=mysqli_query($con,"select * from tbladmin where ID='$adminid'");
-$cnt=1;
-while ($row=mysqli_fetch_array($ret)) {
-
-?>
             <div class="control-group">
-              <label class="control-label">Nom de l'administrateur :</label>
+              <label class="control-label">Nom d'administrateur :</label>
               <div class="controls">
-                <input type="text" class="span11" name="adminname" id="adminname" value="<?php  echo $row['AdminName'];?>" required='true' />
+                <input type="text" class="span11" name="adminname" id="adminname" required='true' />
               </div>
             </div>
             <div class="control-group">
               <label class="control-label">Nom d'utilisateur :</label>
               <div class="controls">
-                <input type="text" class="span11" name="username" id="username" value="<?php  echo $row['UserName'];?>" readonly="true" />
+                <input type="text" class="span11" name="username" id="username" value="saler" readonly='true' />
+                <span class="help-block">Le nom d'utilisateur doit être "saler"</span>
+              </div>
+            </div>
+            <div class="control-group">
+              <label class="control-label">Mot de passe :</label>
+              <div class="controls">
+                <input type="password" class="span11" name="password" id="password" required='true' />
               </div>
             </div>
             <div class="control-group">
               <label class="control-label">Numéro de contact :</label>
               <div class="controls">
-                <input type="text"  class="span11"id="contactnumber" name="contactnumber" value="<?php  echo $row['MobileNumber'];?>" required='true' maxlength='10' patter='[0-9]+'  />
+                <input type="text" class="span11" name="mobileno" id="mobileno" required='true' maxlength='10' pattern='[0-9]+' />
               </div>
             </div>
             <div class="control-group">
               <label class="control-label">Adresse e-mail :</label>
               <div class="controls">
-                <input type="email" class="span11" id="email" name="email" class="form-control1 input-lg" value="<?php  echo $row['Email'];?>" readonly='true' />
+                <input type="email" class="span11" name="email" id="email" required='true' />
               </div>
             </div>
-            <div class="control-group">
-              <label class="control-label">Date d'inscription :</label>
-              <div class="controls">
-                <input type="text" class="span11" value="<?php  echo $row['AdminRegdate'];?>" readonly="true" />
-                </div>
-            </div>
-            
-            <?php } ?>
             <div class="form-actions">
-              <button type="submit" class="btn btn-success" name="submit">Mettre à jour</button>
+              <button type="submit" class="btn btn-success" name="createUser">Créer utilisateur</button>
             </div>
           </form>
         </div>
       </div>
-    
+      
+      <!-- Display existing users -->
+      <div class="widget-box">
+        <div class="widget-title"> <span class="icon"><i class="icon-th"></i></span>
+          <h5>Liste des utilisateurs</h5>
+        </div>
+        <div class="widget-content nopadding">
+          <table class="table table-bordered data-table">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Nom</th>
+                <th>Nom d'utilisateur</th>
+                <th>Contact</th>
+                <th>Email</th>
+                <th>Date d'inscription</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php
+              $ret=mysqli_query($con,"SELECT * FROM tbladmin");
+              $cnt=1;
+              while ($row=mysqli_fetch_array($ret)) {
+              ?>
+              <tr class="gradeX">
+                <td><?php echo $cnt;?></td>
+                <td><?php echo $row['AdminName'];?></td>
+                <td><?php echo $row['UserName'];?></td>
+                <td><?php echo $row['MobileNumber'];?></td>
+                <td><?php echo $row['Email'];?></td>
+                <td><?php echo $row['AdminRegdate'];?></td>
+              </tr>
+              <?php 
+              $cnt=$cnt+1;
+              }?>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   </div>
- </div>
+</div>
 </div>
 <?php include_once('includes/footer.php');?>
 <?php include_once('includes/js.php');?>
