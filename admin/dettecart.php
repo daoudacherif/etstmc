23b6bedac977e0e0c4fff5052dae912430d9d343
daoudacherif <daoudacherif4321@gmail.com>
2025-05-14 20:55:52 +0000

    errors fixed trial

--- admin/dettecart.php
+++ admin/dettecart.php
@@ -2,6 +2,9 @@
 session_start();
 error_reporting(E_ALL);
 include('includes/dbconnection.php');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 // Vérifie que l'admin est connecté
 if (empty($_SESSION['imsaid'])) {
