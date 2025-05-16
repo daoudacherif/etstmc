75e4a9eb48f738e630622d272d8bfe5375f66ade
daoudacherif <daoudacherif4321@gmail.com>
2025-05-16 19:50:39 +0000

    SIDEBAR TRIAL

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -19,11 +19,11 @@ if (strlen($_SESSION['imsaid']==0)) {
   <?php include_once('includes/responsive.php'); ?>
 </head>
 <body>
-  <?php include_once('includes/header.php'); ?>
+      <?php include_once('includes/header.php'); ?>
     <?php include_once('includes/sidebar.php'); ?>
   <!-- Main application container -->
   <div id="app-container">
-    
+
     <!--sidebar-menu-->
 
     <!-- main-container-part -->
