842565e617151af18c2ec34433cb30b810804bf3
daoudacherif <daoudacherif4321@gmail.com>
2025-05-19 17:06:18 +0000

    report fixes

--- admin/daily-repport.php
+++ admin/daily-repport.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
 error_reporting(E_ALL);
+// Affiche toutes les erreurs (à désactiver en production)
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 include('includes/dbconnection.php');
 use Dompdf\Dompdf;
 
