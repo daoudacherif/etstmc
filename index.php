80f7609161a7b16b9e2c07fe16771a49885ae85f
daoudacherif <daoudacherif4321@gmail.com>
2025-05-29 01:11:33 +0000

    fix: update dropdown menu options for improved localization

--- index.php
+++ index.php
@@ -66,7 +66,7 @@
                 <div class="relative inline-block text-left">
                     <div>
                         <button type="button" class="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50 transition-colors" id="menu-button" aria-expanded="false" aria-haspopup="true">
-                            Options
+                            Magasins
                             <svg class="-mr-1 size-5 text-gray-400 transition-transform" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon" id="dropdown-arrow">
                                 <path fill-rule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
                             </svg>
@@ -75,11 +75,8 @@
 
                     <div class="dropdown-menu absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-hidden" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1" id="dropdown-menu">
                         <div class="py-1" role="none">
-                            <a href="https://second.etstmc.com/admin/login.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 transition-colors" role="menuitem" tabindex="-1">Paramètres du compte</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 transition-colors" role="menuitem" tabindex="-1">Support</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 transition-colors" role="menuitem" tabindex="-1">Licence</a>
-                            <div class="border-t border-gray-100"></div>
-                            <button type="button" class="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 transition-colors" role="menuitem" tabindex="-1">Se déconnecter</button>
+                            <a href="https://second.etstmc.com/admin/login.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 transition-colors" role="menuitem" tabindex="-1">Bailobaya</a>
+                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 transition-colors" role="menuitem" tabindex="-1">Matam</a>
                         </div>
                     </div>
                 </div>
