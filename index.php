579a0da98d3ab16a54ea8ef6a1b27f06f8fba7d0
daoudacherif <daoudacherif4321@gmail.com>
2025-05-29 00:54:34 +0000

    fix: enhance dropdown button transition for improved user experience

--- index.php
+++ index.php
@@ -55,11 +55,11 @@
                 
                 <!-- Dropdown Magasins -->
                 <div class="relative dropdown">
-                    <button class="flex items-center text-dark hover:text-primary transition">
+                    <button class="flex items-center text-dark hover:text-primary transition ease-in duration-75">
                         <span>Magasins</span>
                         <i class="fas fa-chevron-down ml-1 text-xs"></i>
                     </button>
-                    <div class="dropdown-menu absolute hidden mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-50 transition ease-in duration-75">
+                    <div class="dropdown-menu absolute hidden mt-2 w-48 bg-white rounded-md shadow-lg py-1 z-50">
                         <a href="https://second.etstmc.com/admin/login.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Bailobaya</a>
                         <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Matam</a>
                         <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Ratoma</a>
