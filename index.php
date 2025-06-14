98cd359494008abed1474b15c0be297dc09a3ad2
daoudacherif <daoudacherif4321@gmail.com>
2025-06-14 23:32:54 +0000

    feat: update mobile dropdown link to point to TMC-3 admin login

--- index.php
+++ index.php
@@ -91,7 +91,7 @@
                     <div class="dropdown-menu absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-hidden" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1" id="dropdown-menu">
                         <div class="py-1" role="none">
                             <a href="https://second.etstmc.com/admin/login.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 transition-colors" role="menuitem" tabindex="-1">Bailobaya</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 transition-colors" role="menuitem" tabindex="-1">Matam</a>
+                            <a href="https://Four.etstmc.com/admin/login.php" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 transition-colors" role="menuitem" tabindex="-1">TMC-3</a>
                         </div>
                     </div>
                 </div>
